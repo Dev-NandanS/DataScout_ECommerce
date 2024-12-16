084771b5630d9c5beefbd19041b6281d6d8b62a1
Dev-NandanS <anandudevnandan7@gmail.com>
2024-12-16 12:18:56 +0530

    main changes

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from app import create_app
-from app.routes import init_routes
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
-app = create_app()
-init_routes(app)
+app = Flask(__name__)
 
-if __name__ == '__main__':
+# MongoDB setup
+client = MongoClient("mongodb://localhost:27017")
+db = client["ecommerce_db"]  # Use your database
+collection = db["products"]  # Use your products collection
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    # Fetch data from MongoDB
+    products = list(collection.find({}, {'_id': 0}))  # Remove MongoDB's default _id field
+    return jsonify(products)
+
+if __name__ == "__main__":
     app.run(debug=True)
